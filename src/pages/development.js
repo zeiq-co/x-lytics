d8cce408d136e944ba41a110bb7cc3da0a1651de
gagansaini1212 <gagansaini1212@gmail.com>
2019-09-30 16:11:04 +0530

    added pages

---
+++ src/pages/development.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const Development = () => <Layout />;
+
+export default Development;
---
+++ src/pages/training.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const Training = () => <Layout />;
+
+export default Training;
